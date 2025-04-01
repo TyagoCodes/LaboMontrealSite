b3f342efb41711b191fb0d4d1513ba2c0f08e0e6
Santiago <santiagox46@hotmail.com>
2025-04-01 01:31:36 -0400

    ajout dun deuxieme ul pour top left car separateur + burger menu dans le futur

--- src/App.tsx
+++ src/App.tsx
@@ -8,18 +8,29 @@ function App() {
         {/* top nav bar*/}  {/* a faire en component*/}
         {/* NavBar trop grand arrive pas a faire plus petit.... pense que cest a cause de taille de mes images*/}
         {/* Trucs bizzarres blanc top left and right jsp pk a fix...*/}
-        <nav className="flex items-center justify-between  bg-neutral-800 text-white px-8  border-b-1">
-            <a href="/">
-                <img src="/MontrealLogo.png" alt="Montreal Logo" className="h-26" />
-            </a>
-            <ul className="flex gap-6">
+        <nav className="flex items-center justify-between  bg-neutral-800 text-white px-8  border-b">
+            <ul className="flex items-center gap-4">
+                <li>
+                    <a href="/">
+                        <img src="/MontrealLogo.png" alt="Montreal Logo" className="h-26" />
+                    </a>
+                </li>
+
+                <li><img src="/bgSep.png" alt="Separator" className="h-9" /></li>
+
+                {/* Eventuellement menu burger ici */}
+            </ul>
+
+            <ul className="flex items-center gap-4">
                 <li>
                     <a href="/search" className="flex items-center gap-2">
-                        <img src="/searchIcon.png" alt="Search Icon" className="h-5" />
+                        <img src="/searchIcon.png" alt="Search Icon" className="h-7" />
                         <span>Search</span>
                     </a>
                 </li>
+
                 <li><img src="/bgSep.png" alt="Image Separator" className="h-9"/></li>
+
                 <li>
                     <a href="/logIn"  className="flex items-center gap-2">
                         <img src="/AccIcon.png" alt="Account Icon" className="h-5"></img>
