097356612eca45a1ba31c80f9929c9707b5f9cc8
Santiago <santiagox46@hotmail.com>
2025-03-31 23:58:38 -0400

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './index.css'
+
+function App() {
+
+
+    return (
+        <>
+        {/* top nav bar*/}
+
+        {/* search part*/}
+        {/* filter part*/}
+        {/* grid for news and newsletter */}
+        </>
+    )
+}
+
+export default App
