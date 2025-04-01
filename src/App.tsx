7272e2def26a2727c07454fd74a7b2de14a7962b
Santiago <santiagox46@hotmail.com>
2025-04-01 04:08:30 -0400

    + sect alertes, newsletter + footer

--- src/App.tsx
+++ src/App.tsx
@@ -20,6 +20,7 @@ function App() {
     return (
         <>
             <NavBar/>
+
             {/* search part*/} {/* a faire en component*/}
             <div className="bg-neutral-700 py-8 px-26">
                 <div className="flex flex-col gap-y-4">
@@ -37,15 +38,17 @@ function App() {
                     </div>
                 </div>
             </div>
+
             {/* filter part*/} {/* a faire en component*/}
             <div className="px-10 py-2 border-b flex flex-row-3">
                 <button className="button-sec3-style" onClick={() => console.log("button 1 clicked")}>Borough</button>
                 <button className="button-sec3-style mx-4" onClick={() => console.log("button 2 clicked")}>Date</button>
                 <button className="button-sec3-style" onClick={() => console.log("button 3 clicked")}>Topic</button>
             </div>
+
             {/* grid for news and newsletter */} {/* a faire en component*/}
-            <div>
-                <ul>
+            <div className="grid grid-flow-col grid-rows-3 gap-4">
+                <ul className="row-span-3 ">
                     {
                         nouvelleList.map((n, i) =>
                             <li key={i} className="border-t border-neutral-600 py-4">
@@ -62,8 +65,32 @@ function App() {
                         )
                     }
                 </ul>
+
+                <div className="block w-[350px] h-[150px] col-span-2 rounded-lg shadow-sm bg-gray-800 p-6">
+                    <h1>Subscribe to alerts</h1>
+                    <p>To receive notices and alerts by e-mail or text message, you must create an account.</p>
+                    <a href="/subcriptionLink" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                        Subscribe
+                        <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true"
+                             xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                  d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                        </svg>
+                    </a>
+                </div>
+
+                <div className="col-span-2 row-span-2 ...">
+                    <h1></h1>
+                </div>
             </div>
+
             {/*  Footer  */}
+            <footer className="bg-neutral-700 py-8 px-26">
+                <div className="flex flex-col gap-y-4">
+                    <h1 className="text-[26px] font-bold">Footer</h1>
+                    <h3 className="text-[15px]">TO DO</h3>
+                </div>
+            </footer>
         </>
     )
 }
