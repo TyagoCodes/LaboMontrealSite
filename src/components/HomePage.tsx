603beec49f890efa07b99498260f361fafb45f20
Santiago <santiagox46@hotmail.com>
2025-04-06 16:35:04 -0400

    ajout d'espacements verticaux pour notices and alerts et subscribe bouton

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -9,7 +9,7 @@ function HomePage() {
     return (
         <>
             <div className="bg-neutral-700 py-8">
-                <div className="flex flex-col max-w-screen-xl mx-auto px-4">
+                <div className="flex flex-col max-w-screen-xl mx-auto px-4 gap-y-5">
                     <h1 className="text-[26px] font-bold">Notices and Alerts</h1>
                     <h3 className="text-[15px]">Find a notice</h3>
                     <div className="flex items-center border-double border-2 border-cyan-600 rounded-full px-3 py-2 w-full max-w-md bg-neutral-700">
@@ -49,10 +49,10 @@ function HomePage() {
 
                     {/*En mobile mode jaimerais envoyer le subscribe bouton en bas a la place de a droite */}
                     <div className="w-1/4 block max-w-screen-xl mx-auto max-h-60 rounded-lg shadow-sm bg-gray-800 p-6 ">
-                        <div className="">
-                            <h1>Subscribe to alerts</h1>
-                            <p>To receive notices and alerts by e-mail or text message, you must create an account.</p>
-                            <Link to="/subcriptionLink" className="inline-flex items-center px-3 py-2 text-sm font-medium text-white bg-blue-700 rounded-lg hover:bg-blue-800">
+                        <div className="flex flex-col max-w-screen-xl mx-auto px-4 gap-y-5">
+                            <h1 className="text-xl text-amber-50 font-extrabold">Subscribe to alerts</h1>
+                            <p className="text-s">To receive notices and alerts by e-mail or text message, you must create an account.</p>
+                            <Link to="/subcriptionLink" className="inline-flex items-center px-3 py-2 text-sm font-bold text-white bg-blue-700 rounded-lg hover:bg-blue-800">
                                 Subscribe
                                 <svg className="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                                     <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
